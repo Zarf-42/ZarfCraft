83ebaeed03798a455c09dc64ec216cdbbd56bdf5
Zarf-42 <smulch@hotmail.com>
2026-02-08 21:11:22 -0800

    Repaired 3D mesh generation
    While following the tutorial at https://www.youtube.com/watch?v=ItAvgeKB0Kk
    
    I forgot to move the commit_mesh() function into the generate_mesh() function. We can now see 3D terrain.

---
+++ chunk.gd
@@ -0,0 +1,165 @@
+class_name Chunk
+extends StaticBody3D
+
+# Following https://www.youtube.com/watch?v=Pfqfr3zFyKI
+
+@export var material: Material
+
+@onready var collision_shape = $TerrainCollision
+@onready var mesh_instance: MeshInstance3D = $TerrainMesh
+
+var voxels: Dictionary[Vector3, Color] = {}
+
+var surface_array: Array = []
+var vertices = PackedVector3Array()
+var normals = PackedVector3Array()
+var colors = PackedColorArray()
+
+# These are the vertice coordinates. The cube's center is at 0,0,0. The cube is 1 unit long, 
+# so each vertice is 0.5 units out from the center. X is left/right, Y is vertical, Z is depth.
+var cube_vertices: Array[Vector3] = [
+	Vector3(-0.5, -0.5, 0.5), # 0, Bottom Left Front
+	Vector3(0.5, -0.5, 0.5), # 1, Bottom Right Front
+	Vector3(0.5, -0.5, -0.5), # 2, Bottom Right Back
+	Vector3(-0.5, -0.5, -0.5), # 3, Bottom Left Back
+	Vector3(-0.5, 0.5, 0.5), # 4,  Top Left Front
+	Vector3(0.5, 0.5, 0.5), # 5, Top Right Front
+	Vector3(0.5, 0.5, -0.5), # 6, Top Right Back
+	Vector3(-0.5, 0.5, -0.5) # 7, Top Left Back
+]
+
+# Allows us to add data "per face" according to the video. I think this is so we don't
+# have to say "Face.1" and stuff.
+enum Face{BOTTOM, FRONT, RIGHT, TOP, LEFT, BACK}
+
+# Using the Vertice Numbers in cube_vertices, decide which face consists of which vertices.
+# The order each vertice is listed in matters. Godot uses Clockwise Winding, so whatever
+# vertice we start on, we need to go clockwise to get to the next one.
+var face_indices: Dictionary[Face, Array] = {
+	Face.FRONT: [[0, 4, 5],[0, 5, 1]], #Checked 12/27/25
+	Face.BACK: [[2, 7, 3],[2, 6, 7]], #Checked 12/27/25
+	Face.LEFT: [[3, 7, 4],[3, 4, 0]], #Checked 12/27/25
+	Face.RIGHT: [[1, 5, 6],[1, 6, 2]], #Checked 12/27/25
+	Face.BOTTOM: [[0, 1, 2],[0, 2, 3]], #Checked 12/27/25
+	Face.TOP: [[4, 7, 6],[4, 6, 5]] #Checked 12/27/25
+}
+
+# Normals determine what direction each face is pointing. Faces pointing away from you
+# don't get drawn.
+var face_normals: Dictionary[Face, Vector3] = {
+	Face.FRONT: Vector3(0, 0, 1),
+	Face.BACK: Vector3(0, 0, -1),
+	Face.LEFT: Vector3(-1, 0, 0),
+	Face.RIGHT: Vector3(1, 0, 0),
+	Face.BOTTOM: Vector3(0, -1, 0),
+	Face.TOP: Vector3(0, 1, 0)
+}
+
+var face_colors: Dictionary[Face, Color] = {
+	Face.BOTTOM: Color.RED,
+	Face.FRONT: Color.ORANGE,
+	Face.RIGHT: Color.YELLOW,
+	Face.TOP: Color.GREEN,
+	Face.LEFT: Color.BLUE,
+	Face.BACK: Color.PURPLE
+}
+
+func _ready() -> void:
+	surface_array.resize(Mesh.ARRAY_MAX)
+
+# I think this is where we determine the order in which chunks get generated. We might be able to
+# define the pattern/shape by which chunks are generated here.
+func generate_data(chunk_size: int, max_height: int, noise: Noise, color_array:Array[Color]):
+	for x in range(chunk_size):
+		for z in range(chunk_size):
+			# New Position, I think, is supposed to be where the next chunk generates.
+			# Also I think we generated chunks from the bottom up.
+			# I.E. new_position = 1, 0, 2 is West 1, Up 0, North 2. Y = 0 here because
+			# we start at the bottom of each chunk and generate upwards? Maybe?
+			var new_position = position + Vector3(x, 0, z)
+			
+			var rand = ((
+				noise.get_noise_2d(new_position.x, new_position.z) + 0.5 * 
+				noise.get_noise_2d(new_position.x * 2, new_position.z * 2) + 0.25 * 
+				noise.get_noise_2d(new_position.x * 4, new_position.z * 4)) / 1.75 + 1) / 2
+
+			var rand_p = pow(rand, 2.1)
+			var height = max_height * rand_p
+
+			if height < position.y: continue
+
+			var local_height = height - position.y
+			for y in range(min(local_height, chunk_size)):
+				voxels[Vector3(x, y, z)] = color_array[y % color_array.size()]
+
+func generate_mesh():
+	if voxels.is_empty(): return
+	for position in voxels:
+		var color = voxels[position]
+		# These If statements help optimize our terrain's meshes. If a cube has a neighbor, we don't
+		# render the face that touches that neighbor. This prevents invisible faces from being computed.
+		if not has_neighbor(voxels, Face.FRONT, position):
+			add_face(Face.FRONT, position, color)
+		if not has_neighbor(voxels, Face.BACK, position):
+			add_face(Face.BACK, position, color)
+		if not has_neighbor(voxels, Face.LEFT, position):
+			add_face(Face.LEFT, position, color)
+		if not has_neighbor(voxels, Face.RIGHT, position):
+			add_face(Face.RIGHT, position, color)
+		if not has_neighbor(voxels, Face.TOP, position):
+			add_face(Face.TOP, position, color)
+		if not has_neighbor(voxels, Face.BOTTOM, position):
+			add_face(Face.BOTTOM, position, color)
+		
+	commit_mesh()
+
+func has_neighbor(data: Dictionary[Vector3, Color], face: Face, position: Vector3):
+	# Somehow this is supposed to see if there's a block next to this face, so we can skip rendering
+	# I think it gets the position of the face that was just added in generate_mesh above.
+	# Then it detects if that position + 1 has a block in it (or minus 1; it uses the Face Normal to determine this)
+	var neighbor_position = position + face_normals[face]
+	if data.has(neighbor_position):
+		return true
+	else:
+		return false
+		
+		#return true
+	#return false
+	# The above is a shortcut; the Return True inside the if statement gets us out of this 
+	# function early, meaning we skip the Return False line. No Else needed. Not sure if I like that.
+
+func add_face(face: Face, vertice_position: Vector3, color: Color) -> void:
+	var indices = face_indices[face]
+	#print(indices)
+	for triangle in indices:
+		for index in triangle:
+			vertices.append(cube_vertices[index] + vertice_position)
+			normals.append(face_normals[face])
+			colors.append(color)
+
+# Takes the data that we generated and puts it in an array; I.E. the vertices, normals, and colors
+# we generated previously.
+func commit_mesh():
+	print("Committing Mesh...")
+	# I believe this takes the values assigned to Vertices, Normals, and Colors, and writes
+	# them out into the Surface Array. Essentially, it assigns those properties (which we
+	# generate elsewhere) to the terrain.
+	surface_array[Mesh.ARRAY_VERTEX] = vertices
+	surface_array[Mesh.ARRAY_NORMAL] = normals
+	surface_array[Mesh.ARRAY_COLOR] = colors
+	
+	#Take the data in Surface Array and run it through the add_surface_from_arrays method?
+	mesh_instance.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
+	mesh_instance.mesh.surface_set_material(0, material)
+	
+	collision_shape.shape = mesh_instance.mesh.create_trimesh_shape()
+	
+	# Now we have a chunk's mesh and its collision mesh. Next, we need to add it to
+	# the correct collision layer, so we can use a RayCast3D from the player to find what coordinate
+	# the ground below them is when they spawn. This will allow us to set the player right
+	# on the ground when they spawn. All we need to do here is get the StaticMesh3D we just
+	# generated, and add it to the correct Collision Layer. Everything else will be handled in the 
+	# player spawn() function.
+	
+	var _static_chunk = mesh_instance.get_parent()
+	# 1/17/2026: It appears that I handled this elsewhere. We assign the Collision Layer elsewhere?
--- world.gd
+++ world.gd
@@ -8,6 +8,7 @@ extends Node3D
 @export var world_size: Vector3 = Vector3(world_vector, world_vector, world_vector)
 # Cutoff defines how dense the random cubes are. Higher numbers equate to less density.
 @export_range(-1, 1) var cutoff: float = 0.1
+@export var colors: Array[Color]
 
 # This references a node that exists in the World scene; once we procedurally generate cubes, we should
 # delete that node and this variable.
@@ -15,10 +16,10 @@ extends Node3D
 
 # This is the primitive that we instance to create our terrain. I believe this supersedes the default_cube
 # above, so we should be able to remove that soon.
-@onready var terrain_primitive_cube: MultiMeshInstance3D = $MultiMeshInstance3D
+@onready var chunk: Chunk = $Chunk
 
 # I think this initializes the array that will later contain the coordinate of every cube in our terrain.
-var terrain_data: Array[Vector3] = []
+var terrain_data: Dictionary[Vector3, Color] = {}
 
 
 func _ready():
@@ -33,23 +34,27 @@ func _unhandled_input(event: InputEvent):
 
 func generate_terrain_finite():
 	var random_generator = FastNoiseLite.new()
+	random_generator.noise_type = FastNoiseLite.TYPE_SIMPLEX
+	random_generator.frequency = 0.003
 	
-	for x in range(world_size.x):
-		for z in range(world_size.z):
-			for y in range(world_size.y):
-				# Generates a number between 0.0 and 1.0.
-				var random = random_generator.get_noise_3d(x, z, y)
-				# If that random number is greater than cutoff (defined in the header), place a cube.
-				if random > cutoff:
-					terrain_data.append(Vector3(x, y, z))
-	remove_child(default_cube)
+	#for x in range(world_size.x):
+		#for z in range(world_size.z):
+			#for y in range(world_size.y):
+				## Generates a number between 0.0 and 1.0.
+				#var random = random_generator.get_noise_3d(x, z, y)
+				## If that random number is greater than cutoff (defined in the header), place a cube.
+				#if random > cutoff:
+					#terrain_data[Vector3(x, y, z)] = colors[y % colors.size()]
+	#remove_child(default_cube)
 	
 	# I think this gets the number of cubes we are about to generate from above, and makes that
 	# amount of memory free and available to use in our MultiMesh instancing thingy. I think the
 	# GPU does this.
-	terrain_primitive_cube.multimesh.instance_count = terrain_data.size()
+	chunk.generate_data(world_size.x, world_size.y, random_generator, colors)
+	chunk.generate_mesh()
+	#chunk.multimesh.instance_count = terrain_data.size()
 	
-	for i in range(terrain_primitive_cube.multimesh.instance_count):
-		# This was not deeply explained in the tutorial. I think this actually sets the position of each
-		# cube (terrain_primitive_cube) based on terrain_data[i].
-		terrain_primitive_cube.multimesh.set_instance_transform(i, Transform3D(Basis(), terrain_data[i]))
+	#for i in range(chunk.multimesh.instance_count):
+		## This was not deeply explained in the tutorial. I think this actually sets the position of each
+		## cube (terrain_primitive_cube) based on terrain_data[i].
+		#chunk.multimesh.set_instance_transform(i, Transform3D(Basis(), terrain_data[i]))

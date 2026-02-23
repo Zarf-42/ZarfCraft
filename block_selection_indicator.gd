1852c8df64a66b7364e29bd78b4fd9a587074767
Zarf-42 <smulch@hotmail.com>
2026-02-23 09:58:54 -0800

    ???
    Git is STILL confusing

--- block_selection_indicator.gd
+++ block_selection_indicator.gd
@@ -2,7 +2,8 @@ extends MeshInstance3D
 # Largely used for defining how the texture on the BlockSelectionIndicator behaves.
 
 #@onready var cursor_texture: CompressedTexture2D = preload("res://res/3d_cursor.png")
-const _3D_CURSOR = preload("uid://cqsdyoaagqhnl")
+
+const _3D_CURSOR = preload("uid://ov0tcu67dk28")
 
 func _ready():
 	pass
